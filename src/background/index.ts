ea393ebd841ac1e7442835a617b7319f917267a3
J03L R0J0 <rojo@analogue.app>
2020-03-01 11:23:37 -0800

    ⚡️ backround external message listener [wip]

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,5 @@
+/*global chrome*/
+
 import { createStore } from 'redux';
 import rootReducer from './reducers';
 
@@ -5,12 +7,26 @@ import { wrapStore } from 'webext-redux';
 
 const store = createStore(rootReducer, {});
 
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function() {
   // Send a message to the active tab
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+    chrome.tabs.sendMessage(activeTab.id, {message: "clicked_browser_action"});
   });
 });
+
+// middleware, can only listen for external messages in background page:
+// https://stackoverflow.com/questions/18835452/chrome-extension-onmessageexternal-undefined
+const authListener = (request) => {
+  alert("AUTH LISTENER CALLED FROM BACKGROUND")
+  // Send a message to the active tab to trigger redux store of token
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    var activeTab = tabs[0];
+    alert(activeTab.id)
+    alert(request.token)
+    chrome.tabs.sendMessage(activeTab.id, request);
+  })
+}
+chrome.runtime.onMessageExternal.addListener(authListener)
 
 wrapStore(store);
--- src/contents/all/components/app/App.tsx
+++ src/contents/all/components/app/App.tsx
@@ -18,20 +18,31 @@ const App = () => {
 
   const browserActionListener = (request, sender) => {
     // sender.id is id of chrome extension
+    console.log("BROWSER ACTION LISTENER CALLED", request)
+    console.log("USER", user)
+    console.log('ext id', sender.id)
     if (request.message === "clicked_browser_action") {
-      if (!user) {
+      if (!user && !user.token) {
         window.open("http://localhost:3000/login", "_blank");
-        dispatch({ type: 'SET_USER_TOKEN', token: sender.id })
-        // TODO, get JWT from web app
       }
       dispatch({ type: 'TOGGLE_MODAL', show: true })
     }
   }
 
+  const authListener = (request) => {
+    console.log("AUTH LISTENER CALLED", request)
+    if (request.message === "auth_user") {
+      dispatch({ type: 'SET_USER_TOKEN', token: request.token })
+    }
+  }
+
   useEffect(() => {
     chrome.runtime.onMessage.addListener(browserActionListener)
+    chrome.runtime.onMessage.addListener(authListener)
+
     return () => {
       chrome.runtime.onMessage.removeListener(browserActionListener)
+      chrome.runtime.onMessage.removeListener(authListener)
     }
   }, [user]);
 
