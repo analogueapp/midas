20bab8b363a47f60fb9a51fdbeac1c40b3d1c6bd
J03L R0J0 <rojo@analogue.app>
2020-02-29 16:11:56 -0800

    ⚡️ Popup triggers show

--- src/background/reducers/show.js
+++ src/background/reducers/show.js
@@ -1,9 +1,9 @@
-const initialState = true;
+const initialState = false;
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case 'TOGGLE_MODAL':
-      return !state;
+      return action.show ? action.show : !state;
     default:
       return state;
   }
--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { hot } from 'react-hot-loader/root';
 
@@ -9,12 +9,13 @@ const App = () => {
   const show = useSelector(state => state.show);
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   console.log('Component mounted');
-  //   return () => {
-  //     console.log('Component will be unmount');
-  //   }
-  // }, []);
+  useEffect(() => {
+    // component mount
+    dispatch({ type: 'TOGGLE_MODAL', show: true })
+    return () => {
+      // component unmount
+    }
+  }, []);
 
   return (
     <div className="app">
