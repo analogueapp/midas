11b9dfb43d92320aa0ef1fb68b440d26df0f3694
J03L R0J0 <rojo@analogue.app>
2020-03-14 13:40:22 -0700

    🐞 Refactor Knot to function for react-flip-move

--- src/contents/all/components/knots/Knot/Knot.tsx
+++ src/contents/all/components/knots/Knot/Knot.tsx
@@ -5,12 +5,14 @@ import { Timeline } from 'antd';
 import './Knot.scss';
 import '../Trix.scss';
 
-const Knot = props => (
-  <Timeline.Item className={`knot ${props.isLast ? "ant-timeline-item-last" : ""}`}>
-    <div className={`knotCard ${props.knot.private ? "private" : ""}`}>
-      <div className="trix-content" dangerouslySetInnerHTML={{__html: props.knot.body}} />
-    </div>
-  </Timeline.Item>
-)
+function Knot(props) {
+  return (
+    <Timeline.Item className={`knot ${props.isLast ? "ant-timeline-item-last" : ""}`}>
+      <div className={`knotCard ${props.knot.private ? "private" : ""}`}>
+        <div className="trix-content" dangerouslySetInnerHTML={{__html: props.knot.body}} />
+      </div>
+    </Timeline.Item>
+  )
+}
 
 export default Knot;
