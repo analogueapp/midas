0585993dfead0f529e0d77bc0a62bdafe46c8073
J03L R0J0 <rojo@analogue.app>
2020-03-14 21:17:14 -0700

    ⚡️ Add alternative editor

--- src/contents/all/components/knots/KnotInput/KnotInput.tsx
+++ src/contents/all/components/knots/KnotInput/KnotInput.tsx
@@ -1,28 +1,30 @@
 import React, { useState } from 'react';
-
-import { Timeline, Input } from "antd";
+import { Timeline } from "antd";
+import RichTextEditor from 'react-rte/lib/RichTextEditor';
 
 import "../Knot/Knot.scss"
 import "./KnotInput.scss";
 
 const KnotInput = props => {
 
-  const [body, setBody] = useState(props.knot ? props.knot.body : "")
+  const [body, setBody] = useState(RichTextEditor.createEmptyValue())
 
-  const onChange = ({ target: { value } }) => setBody(value)
+  const onChange = (value) => setBody(value)
 
   return (
     <Timeline.Item className={`knot ${props.hasKnots ? "" : "ant-timeline-item-last"}`}>
       <div className="knotCard">
         <div className="knotEditorWrapper">
           <div className="knotEditor">
-            <Input.TextArea
-              autoSize
-              value={body}
-              autoFocus={props.autoFocus}
-              onChange={onChange}
-              placeholder={props.hasKnots ? "Add another note..." : "Add a note..."}
-            />
+            {props.show &&
+              <RichTextEditor
+                autoFocus
+                toolbarConfig={{ display: [] }}
+                value={body}
+                onChange={onChange}
+                placeholder={props.hasKnots ? "Add another note..." : "Add a note..."}
+              />
+            }
           </div>
         </div>
       </div>
