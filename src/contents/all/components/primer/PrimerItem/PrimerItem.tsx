20d6abd1c00108471bc50fb7a58885acac01b199
J03L R0J0 <rojo@analogue.app>
2020-03-16 12:56:13 -0700

    🔥 Refactor PrimerItem to functional component

--- src/contents/all/components/primer/PrimerItem/PrimerItem.tsx
+++ src/contents/all/components/primer/PrimerItem/PrimerItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { List, Button } from 'antd';
 import { Primer, Log } from '../../../global/types';
 
@@ -12,92 +12,83 @@ import './PrimerItem.scss'
 
 interface Props {
   // updateCurrentPrimers?: (currentSelected: boolean) => void
-  collection: string
-  selectable: boolean
-  selected: boolean
-  primer: Primer
   log: Log
+  primer: Primer
+  collection?: string
+  selectable?: boolean
+  selected?: boolean // ? denotes optional prop in TS
 }
 
-interface State {
-  selected: boolean
-}
+const PrimerItem = (props: Props) => {
 
-class PrimerItem extends React.Component<Props, State> {
-  
-  state = {
-    selected: this.props.selected ? true : false,
-  }
+  const [selected, setSelected] = useState(props.selected ? true : false)
 
-  togglePrimer = (e) => {
+  const togglePrimer = (e) => {
     e.preventDefault()
     e.stopPropagation()
-    const currentSelected = this.state.selected;
-    this.setState({ selected: !currentSelected })
+    const currentSelected = selected;
+    setSelected(!selected)
 
-    if (this.state.selected) {
+    if (selected) {
       // todo add to primer
-      // agent.Primers.removeLog(this.props.primer.slug, this.props.log.id).then(
-      //   res => this.props.updateCurrentPrimers(currentSelected)
+      // agent.Primers.removeLog(props.primer.slug, props.log.id).then(
+      //   res => props.updateCurrentPrimers(currentSelected)
       // )
     } else {
-      // agent.Primers.addLog(this.props.primer.slug, this.props.log.id).then(
-      //   res => this.props.updateCurrentPrimers(currentSelected)
+      // agent.Primers.addLog(props.primer.slug, props.log.id).then(
+      //   res => props.updateCurrentPrimers(currentSelected)
       // )
     }
   }
 
-  render() {
-
-    if (this.props.collection) {
-      return (
-        <List.Item className={`primerItem collection ${this.props.selectable ? "selected" : ""}`}>
-          <div className="imgWrapper">
-            {mediumIcons[this.props.collection]}
-          </div>
-
-          <h5 className="title">{this.props.collection}</h5>
-
-          {this.props.selectable &&
-            <div className="addBtn">
-              <Button><Icon type="check" /></Button>
-            </div>
-          }
-        </List.Item>
-      )
-    }
-
+  if (props.collection) {
     return (
-      <List.Item
-        className={`primerItem ${this.state.selected ? "selected" : ""}`}
-        onClick={this.props.selectable ? this.togglePrimer : null}
-      >
+      <List.Item className={`primerItem collection ${props.selectable ? "selected" : ""}`}>
         <div className="imgWrapper">
-          <ProgressiveImage
-            src={this.props.primer.image}
-            placeholder={placeholderImg}
-          >
-            {(src, loading) => (
-              <img src={src} alt={this.props.primer.title} />
-            )}
-          </ProgressiveImage>
+          {mediumIcons[props.collection]}
         </div>
 
-        <h5 className="title">{this.props.primer.title}</h5>
+        <h5 className="title">{props.collection}</h5>
 
-        <ul className="ant-list-item-action">
-          {this.props.primer.shared && <li><UsergroupAddOutlined /></li>}
-          {this.props.primer.private && <li><LockOutlined /></li>}
-        </ul>
-
-        {this.props.selectable &&
+        {props.selectable &&
           <div className="addBtn">
-            <Button>{this.state.selected ? <CheckOutlined /> : "Add"}</Button>
+            <Button><Icon type="check" /></Button>
           </div>
         }
       </List.Item>
     )
   }
+
+  return (
+    <List.Item
+      className={`primerItem ${selected ? "selected" : ""}`}
+      onClick={props.selectable ? this.togglePrimer : null}
+    >
+      <div className="imgWrapper">
+        <ProgressiveImage
+          src={props.primer.image}
+          placeholder={placeholderImg}
+        >
+          {(src, loading) => (
+            <img src={src} alt={props.primer.title} />
+          )}
+        </ProgressiveImage>
+      </div>
+
+      <h5 className="title">{props.primer.title}</h5>
+
+      <ul className="ant-list-item-action">
+        {props.primer.shared && <li><UsergroupAddOutlined /></li>}
+        {props.primer.private && <li><LockOutlined /></li>}
+      </ul>
+
+      {props.selectable &&
+        <div className="addBtn">
+          <Button>{selected ? <CheckOutlined /> : "Add"}</Button>
+        </div>
+      }
+    </List.Item>
+  )
 }
 
 export default PrimerItem
