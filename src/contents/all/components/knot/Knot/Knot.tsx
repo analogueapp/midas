40bd7d53c3c93b08aef5b08d071803e2c77f9cd4
J03L R0J0 <rojo@analogue.app>
2020-03-17 13:07:27 -0700

    ✨ Add knot meta + timeAgo

--- src/contents/all/components/knot/Knot/Knot.tsx
+++ src/contents/all/components/knot/Knot/Knot.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Moment from 'react-moment';
 
 import { Timeline } from 'antd';
 
@@ -9,6 +10,19 @@ const Knot = (props) => (
   <Timeline.Item className={`knot ${props.isLast ? "ant-timeline-item-last" : ""}`}>
     <div className={`knotCard ${props.knot.private ? "private" : ""}`}>
       <div className="trix-content" dangerouslySetInnerHTML={{__html: props.knot.body}} />
+    </div>
+    <div className="knotMeta">
+      <Moment
+        filter={(value) =>
+          value.replace(/^a few seconds ago/g, 'just now')
+          .replace(/^a /g, '1 ')
+          .replace(/^an /g, '1 ')
+          .replace("minute", 'min')
+        }
+        fromNow
+      >
+        {props.knot.postedAt}
+      </Moment>
     </div>
   </Timeline.Item>
 )
