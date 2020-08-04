ff02f815da11334f6f7aa841b9b0e5c3af720b4d
spencerjsmall <spencer_small@brown.edu>
2020-08-04 15:23:58 -0700

    Fixed Segment.load

--- src/background/index.ts
+++ src/background/index.ts
@@ -20,7 +20,7 @@ const store = createStore(rootReducer, {})
 
 var stream = require('getstream');
 
-window.analytics.load('5misG1vVKILgvkxtM7suBhUouTZBxbJ5')
+Segment.load('5misG1vVKILgvkxtM7suBhUouTZBxbJ5')
 
 const injectContentScript = (message = null) => {
   // first, query to see if content script already exists in active tab
